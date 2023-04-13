ad8545ef93464304a8f6d0296f338307ec8ab770
KhangB1906686 <97388634+KhangB1906686@users.noreply.github.com>
2023-04-13 21:38:56 +0700

    Add files via upload

--- pages/main/baiviet.php
+++ pages/main/baiviet.php
@@ -6,66 +6,119 @@ $row_bv_title = mysqli_fetch_array($query_bv);
 ?>
 
 <link rel="stylesheet" href="css/baiviet.css">
-<style > .slider{
-    display: none;
-}
 
-</style>
+<?php
+while ($row_bv = mysqli_fetch_array($query_bv_all)) {
+?>
 
-<div class="bv">
-     <!-- <div class="top-slide">
-         <div class="link-newfeed">
-            <b>Tin Tức Khác</b>
-        </div>
-    </div>  -->
-    <div class="right-slide">
-
-            <div class="link-newfeed">
-                <b>Tin Tức Khác</b>
-            </div>
-            <div class="newfeed">
-                <div class="slide-wrapper">
-                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=8"> 
-                        <div class="slide"><img src="/images/tintuc/Baiviet1.jpg" width="250px" height="250px" ></div> </a>
-                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=9"> 
-                        <div class="slide"><img src="/images/tintuc/Baiviet2.jpg" width="250px" height="250px"></div> </a>
-                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=10"> 
-                        <div class="slide"><img src="/images/tintuc/Baiviet3.jpg" width="250px" height="250px"></div> </a>
-                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=12"> 
-                        <div class="slide"><img src="/images/tintuc/baiviet5.jpg" width="250px" height="250px"></div> </a>
-                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=14"> 
-                        <div class="slide"><img src="/images/tintuc/baiviet6.jpg" width="250px" height="250px"></div> </a>
-                </div>
-            </div>  
-        </div>  
-    <?php
-    while ($row_bv = mysqli_fetch_array($query_bv_all)) {
-    ?>
+    <body>
         <div class="baiviet_khung">
-            <h2><?php echo $row_bv['tenbaiviet'] ?></h2>
-            <p class="title-product"><?php echo $row_bv['tomtat'] ?></p>
-            <p style="margin: 10px;" class="title-product"><?php echo $row_bv['noidung'] ?></p>
+            <div class="content-baiviet">
+                <h1><?php echo $row_bv['tenbaiviet'] ?></h2>
+                    <p class="noidung-baiviet"><?php echo $row_bv['noidung'] ?></p>
+            </div>
+            <div class="content-new">
+                <h3 class="title-baiviet">Tin Tức Khác</h3>
+
+                <!-- <div class="newfeed">
+                <div class="slide-wrapper">
+                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=8">
+                        <div class="slide"><img src="/images/tintuc/Baiviet1.jpg" ></div>
+                    </a>
+                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=9">
+                        <div class="slide"><img src="/images/tintuc/Baiviet2.jpg" ></div>
+                    </a>
+                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=10">
+                        <div class="slide"><img src="/images/tintuc/Baiviet3.jpg" ></div>
+                    </a>
+                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=12">
+                        <div class="slide"><img src="/images/tintuc/baiviet5.jpg" ></div>
+                    </a>
+                    <a href="http://localhost:3000/index.php?quanly=baiviet&id=14">
+                        <div class="slide"><img src="/images/tintuc/baiviet6.jpg"></div>
+                    </a>
+                </div>
+            </div> -->
+            </div>
+
+            <div class="slideshow-container">
+
+
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>Slide 1</h2>
+                        <p>Nullam euismod, quam sed ultricies lacinia, odio nibh dapibus odio, ut maximus ex magna vitae ipsum.</p>
+                        <button class="btn btn-primary">View More</button>
+                    </div>
+                    <<img src="/images/tintuc/Baiviet1.jpg">
+                </div>
+
+
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>EXYNOS 1330</h2>
+                        <p>EXYNOS 1330 VÀ EXYNOS 1380 CHÍNH THỨC RA MẮT: THÔNG SỐ ẤN TƯỢNG, HỖ TRỢ MẠNG 5G!</p>
+                        <a href="http://localhost:3000/index.php?quanly=baiviet&id=8">
+                            <button class="btn btn-primary">View More</button>
+                        </a>
+                    </div>
+                    <<img src="/images/tintuc/Baiviet1.jpg">
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>IPHONE 16 ULTRA</h2>
+                        <p>KỲ VỌNG IPHONE 16 ULTRA CONCEPT: THIẾT KẾ ẤN TƯỢNG, CHIP APPLE A18 BIONIC MẠNH MẼ</p>
+                        <a href="http://localhost:3000/index.php?quanly=baiviet&id=9">
+                            <button class="btn btn-primary">View More</button>
+                        </a>
+
+                    </div>
+                    <img src="/images/tintuc/Baiviet2.jpg">
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>XIAOMI 13</h2>
+                        <p>XIAOMI 13 LITE ĐÃ CÓ TRÊN GEEKBENCH, CÙNG KHÁM PHÁ ĐIỂM HIỆU NĂNG NHÉ!</p>
+                        <a href="http://localhost:3000/index.php?quanly=baiviet&id=10">
+                            <button class="btn btn-primary">View More</button>
+                        </a>
+                    </div>
+                    <img src="/images/tintuc/Baiviet3.jpg">
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>SONY WF-1000XM5</h2>
+                        <p>SONY WF-1000XM5 CÓ MẶT TRÊN TRANG CHỨNG NHẬN NỔI TIẾNG, LỘ RA THÔNG SỐ KẾT NỐI</p>
+                        <a href="http://localhost:3000/index.php?quanly=baiviet&id=12">
+                            <button class="btn btn-primary">View More</button>
+                        </a>
+                    </div>
+                    <img src="/images/tintuc/baiviet5.jpg">
+                </div>
+                <div class="slide">
+                    <div class="slide-content">
+                        <h2>GOOGLE TỐI ƯU HÓA GIAO DIỆN GMAIL CHO GALAXY Z FOLD4</h2>
+                        <p>GOOGLE TỐI ƯU HÓA GIAO DIỆN GMAIL CHO GALAXY Z FOLD4, NHÌN THOẢI MÁI VÀ ĐẸP HƠN</p>
+                        <a href="http://localhost:3000/index.php?quanly=baiviet&id=14">
+                            <button class="btn btn-primary">View More</button>
+                        </a>
+                    </div>
+                    <img src="/images/tintuc/baiviet6.jpg">
+                </div>
+            </div>
         </div>
     <?php
-    }
+}
     ?>
+    <script src="js/baiviet.js"></script>
+    </body>
+    <style>
+        .l-10 {
+            flex: 100%;
+            max-width: 100%;
+        }
 
-</ul>
-</div>
-
-
-    <!-- <div class="top-slide">
-        <div class="link-newfeed">
-            <a href="">Bài Viết Khác</a>
-        </div>
-</div>
-<div class="right-slide">
-        
-        <div class="newfeed">
-            <div class="slide-wrapper">
-                <div class="slide"><img src="/images/Baiviet1.jpg" width="250px" height="250px" ></div>
-                <div class="slide"><img src="/images/Baiviet2.jpg" width="250px" height="250px"></div>
-                <div class="slide"><img src="/images/Baiviet3.jpg" width="250px" height="250px"></div>
-            </div>
-        </div>  
-    </div> -->
+        .sidebar {
+            display: none;
+        }
+    </style>
--- pages/main/dangnhap.php
+++ pages/main/dangnhap.php
@@ -1,59 +1,117 @@
 <?php
-if(isset($_POST['dangnhap'])) {
+if (isset($_POST['dangnhap'])) {
     $email = $_POST['email'];
     $matkhau = md5($_POST['password']);
-    $sql = "SELECT * FROM dangky WHERE email = '".$email."' AND matkhau = '".$matkhau."' LIMIT 1";
+    $sql = "SELECT * FROM dangky WHERE email = '" . $email . "' AND matkhau = '" . $matkhau . "' LIMIT 1";
     $row = mysqli_query($mysqli, $sql);
     $count = mysqli_num_rows($row);
-    if($count > 0) {
+    if ($count > 0) {
         $row_data = mysqli_fetch_array($row);
         $_SESSION['dangky'] = $row_data['tenkhachhang'];
         $_SESSION['email'] = $row_data['email'];
         $_SESSION['id_dangky'] = $row_data['tenkhachhang'];
-        header("Location: index.php?quanly=index");
-        
+        header("Location: index.php?quanly=giohang");
     } else {
         echo '<p style="color: red;">Email hoặc Mật khẩu sai, vui lòng nhập lại.</p>';
-       
     }
 }
 
 ?>
+
+<?php
+if (isset($_POST['dangky'])) {
+    $tenkhachhang = $_POST['hovaten'];
+    $email = $_POST['email'];
+    $dienthoai = $_POST['dienthoai'];
+
+    $matkhau = md5($_POST['matkhau']);
+    $diachi = $_POST['diachi'];
+    $sql_dangky = mysqli_query($mysqli, "INSERT INTO dangky(tenkhachhang,email,diachi,matkhau,dienthoai) VALUE('" . $tenkhachhang . "', '" . $email . "', '" . $diachi . "', '" . $matkhau . "', '" . $dienthoai . "')");
+    if ($sql_dangky) {
+        echo '<p style="color: green;">Bạn đã đăng ký thành công.</p>';
+        $_SESSION['dangky'] = $tenkhachhang;
+        $_SESSION['email'] = $email;
+
+        $_SESSION['id_khachhang'] = mysqli_insert_id($mysqli);
+        header('Location:index.php?quanly=dangnhap');
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Login Account</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        
-        <link rel="stylesheet" href="css/dangnhap.css">
-        <style>
-            .slider{
-                display: none;
-            }
-        </style>
-    </head>
-    <body>
+
+<head>
+    <title>Login Account</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+    <link rel="stylesheet" href="css/dangnhap.css">
+    <style>
+        .slider {
+            display: none;
+        }
+    </style>
+</head>
+
+<body>
     <form action="" autocomplete="off" method="POST">
-    <div class="container_form">
-    <a class="name-form">ĐĂNG NHẬP</a>
 
-      <div class="form">
-          <input type="text" id="email" class="form__input" name="email" autocomplete="off" placeholder=" ">
-          <label for="email" class="form__label">Tài Khoản Email</label>
-      </div>
-      <div class="form">
-          <input type="password" id="password" name="password" class="form__input" autocomplete="off" placeholder=" ">
-          <label for="email" class="form__label">Mật Khẩu</label>
-      </div>
-          <input type="submit" name="dangnhap" value="Đăng nhập" class="submit">
-          <a href="index.php?quanly=quenmatkhau" class="qmk">Quên Mật Khẩu</a>
-        
-      </div>
+        <!-- <div class="container_form">
+            <a class="name-form">ĐĂNG NHẬP</a>
+
+            <div class="form">
+                <input type="text" id="email" class="form__input" name="email" autocomplete="off" placeholder=" ">
+                <label for="email" class="form__label">Tài Khoản Email</label>
+            </div>
+            <div class="form">
+                <input type="password" id="password" name="password" class="form__input" autocomplete="off" placeholder=" ">
+                <label for="email" class="form__label">Mật Khẩu</label>
+            </div>
+            <input type="submit" name="dangnhap" value="Đăng nhập" class="submit">
+            <a href="index.php?quanly=quenmatkhau" class="qmk">Quên Mật Khẩu</a>
+            <p>Bạn chưa có tài khoản? <a href="#" id="register-btn">Đăng ký</a></p> -->
+
+        <div class="login-container">
+            <h2>Đăng nhập</h2>
+            <form>
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+                <label for="password">Mật khẩu:</label>
+                <input type="password" id="password" name="password" required>
+                <button type="submit" name="dangnhap" value="Đăng nhập" >Đăng nhập</button>
+            </form>
+            <p>Bạn chưa có tài khoản? <a href="#" id="register-btn">Đăng ký</a></p>
+            <p>Bạn quên mật khẩu? <a href="index.php?quanly=quenmatkhau" class="qmk">Quên Mật Khẩu</a></p>
+
+        </div>
+
+    </form>
+    <form action="" autocomplete="off" method="POST">
+        <div class="register-popup" id="register-popup">
+            <div class="popup-content">
+                <h2>Đăng ký</h2>
+                <form>
+                    <label for="fullname">Họ và tên:</label>
+                    <input type="text" id="hovaten" name="hovaten" required>
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                    <label for="password">Mật khẩu:</label>
+                    <input type="password" id="matkhau" name="matkhau" required>
+                    <label for="confirm-password">Nhập lại mật khẩu:</label>
+                    <input type="password" id="repassword" name="repassword" required>
+                    <label for="address">Địa chỉ:</label>
+                    <input type="text" id="diachi" name="diachi" required>
+                    <label for="phone">Số điện thoại:</label>
+                    <input type="tel" id="dienthoai" name="dienthoai" pattern="[0-9]{10}" required>
+                    <button type="submit" class="submit" id="dangky" name="dangky">Đăng ký</button>
+                    <button type="button" id="close-btn">Close Form</button>
+                </form>
+            </div>
+        </div>
+    </form>
+    <script src="js/dangnhap.js"></script>
 
 
-      </div>
-
-
-    </body>
+</body>
 
 </html>
